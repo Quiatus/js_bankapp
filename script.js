89f3ecbfd58d7c7b28cf35bdd6733d4f12d71696
Quiatus <Quiatus@gmail.com>
2024-05-09 20:52:24 +0200

    add maps

--- script.js
+++ script.js
@@ -63,7 +63,8 @@ const inputClosePin = document.querySelector('.form__input--pin');
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1
 
 //movements.forEach((move, i) => move > 0 ? console.log(`${i+1}. DEP: ${move}`) : console.log(`${i+1}. WIT: ${Math.abs(move)}`))
 
@@ -85,3 +86,18 @@ const displayMovements = (movements) => {
 }
 
 displayMovements(account1.movements)
+
+const createUserName = (accounts) => {
+  accounts.forEach(acc => acc.username = acc.owner.toLowerCase().split(' ').map(item => item[0]).join(''))
+}
+createUserName(accounts)
+console.log(accounts)
+
+// const movUSD = movements.map(item => item * eurToUsd)
+
+// console.log(movements)
+// console.log(movUSD)
+
+// const movDesc = movements.map((mov, i) => `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`)
+
+// console.log(movDesc)
