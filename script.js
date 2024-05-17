729e618a4cac51ddd98944254dded5575ec3e6d7
Quiatus <Quiatus@gmail.com>
2024-05-17 16:56:06 +0200

    add close account

--- script.js
+++ script.js
@@ -147,6 +147,20 @@ btnTransfer.addEventListener('click', (e) => {
 })
 
 
+btnClose.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username)
+    accounts.splice(index, 1)
+
+    inputCloseUsername.value = inputClosePin.value = ''
+    inputClosePin.blur()
+    containerApp.style.opacity = 0
+    labelWelcome.textContent = 'Log in to get started'
+  }
+})
+
 // Training:
 
 // const movUSD = movements.map(item => item * eurToUsd)
